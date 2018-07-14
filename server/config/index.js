14b61f041c6b3102ac80e34cbc7fb0a48594f257
MarioTury <mariotury@outlook.com>
2018-07-14 16:29:42 +0300

    Update index.js
    Changed db connection from localhost to mlab connection drivers

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,8 @@
 // Config object for the database
 // Deployment and test database
 const config ={
-	db: 'mongodb://127.0.0.1/pixel_app_db',
+	db: 'mongodb://mariotury:IFjirGhifyugFa4@ds121251.mlab.com:21251/pixel_test',
+	//db: 'mongodb://127.0.0.1/pixel_app_db',
 	testDb: 'mongodb://127.0.0.1/pixel_app_db_test'
 };
 
